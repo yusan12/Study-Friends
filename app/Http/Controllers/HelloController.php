3eaffb738e2439d1d9a270886b73704e3bda3ae3
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-10-14 09:55:47 +0900

    practice Laravel

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,41 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-        global $head, $style,$body, $end;
-        $head = '<html></html>';
-        $style = <<<EOF
-        <style>
-        body {font-size:16px; color:#999; }
-        h1 { font-size:100px; text-align:right; color:#eee;
-            margin:-40px 0px -50px 0px; }
-        </style>
-        EOF;
-        $body = '</head><body>';
-        $end = '</body></html>';
-        function tag($tag, $txt) {
-            return "<{$tag}>" . $txt . "</{$tag}>";
-        }
+use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index() {
-        global $head, $style,$body, $end;
-        $html = $head . tag('title', 'Hello/Index') . $style .
-            $body
-            . tag('h1', 'index') . tag('p','this is Index page')
-            . '<a href="/hello/other">go to Other page</a>'
-            . $end;
-        return $html;
-    }
+    public function index(Request $request, Response $response) {
 
-    public function other() {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title', 'Hello/Other') . $style .
-            $body
-            . tag('h1','Other') . tag('p','this is Other page')
-            . $end;
-        return $html;
+        $html = <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+        body {font-size:16px; color:#999; }
+        h1 { font-size:120px; text-align:right; color:#fafafa;
+            margin:-50px 0px -120px 0px; }
+        </style>
+        </head>
+        <body>
+            <h1>Hello</h1>
+            <h3>Request</h3>
+            <pre>{$request}</pre>
+            <h3>Response</h3>
+            <pre>{$response}</pre>
+        </body>
+        </html>
+        EOF;
+                $response->setContent($html);
+                return $response;
     }
 }

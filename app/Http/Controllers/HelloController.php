25eee619d6fa5388878bbcc5c7f2fde408062ff8
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-10-19 10:51:07 +0900

    #201019-laravel-practice

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,12 +9,7 @@ class HelloController extends Controller
 {
     public function index()
 {
-    $data = [
-        ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
-        ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
-        ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy'],
-    ];
-    return view('hello.index', ['data'=>$data]);
+    return view('hello.index', ['message'=>'Hello!']);
 }
 
     public function post(Request $request)
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -17,9 +17,9 @@
 
     @section('content')
         <p>ここが本文のコンテンツです。</p>
-        <ul>
-        @each('components.item', $data, 'item')
-        </ul>
+        <p>Controller value<br>'message' = {{$message}}</p>
+        <p>ViewComposer value<br>'view_message' = {{$view_message}}
+            </p>
     @endsection
 
         

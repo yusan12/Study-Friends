fd6d824a13de09c980d23d4e5b49eeff7faaccbd
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-10-21 14:09:00 +0900

    errer message and keep value

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -16,9 +16,27 @@
     @endsection
 
     @section('content')
-   <p>ここが本文のコンテンツです。</p>
-   <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-   <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+        <p>{{$msg}}</p>
+        @if (count($errors) > 0)
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+        <form action="/hello" method="post">
+        <table>
+            @csrf
+            <tr><th>name: </th><td><input type="text" name="name"
+                value="{{old('name')}}"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail"
+                value="{{old('mail')}}"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age"
+                value="{{old('age')}}"></td></tr>
+            <tr><th></th><td><input type="submit"
+                value="send"></td></tr>
+        </table>
+        </form>
 @endsection
-
-        
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@
 use App\Http\Middleware\HelloMiddleware;
 
 Route::get('hello', 'HelloController@index');
-
 Route::post('hello', 'HelloController@post');
 
 Route::get('/', function () {

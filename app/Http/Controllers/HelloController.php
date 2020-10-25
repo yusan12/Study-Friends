d47da1ee53224187b219a071f4792e76e33b5668
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-10-25 11:53:55 +0900

    #201025-laravel-practice

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
@@ -14,12 +16,16 @@ class HelloController extends Controller
 
     public function post(Request $request)
     {
-        $validate_rule = [
+        $validator = Validator::make($request->all(), [
             'name' => 'required',
             'mail' => 'email',
             'age' => 'numeric|between:0,150',
-        ];
-        $this->validate($request, $validate_rule);
+        ]);
+        if ($validator->fails()) {
+            return redirect('/hello')
+                            ->withErrors($validator)
+                            ->withInput();
+        }
         return view('hello.index', ['msg'=>'正しく入力されました！']);
     }
 }

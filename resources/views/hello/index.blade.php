1c7d04c0275c4922dd61243af4eaa1158e759234
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-10-27 07:55:11 +0900

    #201027-laravel-practice

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -23,20 +23,14 @@
         <form action="/hello" method="post">
         <table>
             @csrf
-            @error('name')
-                <tr><th>ERROR</th>
-                <td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>name: </th><td><input type="text"
-                name="name" value="{{old('name')}}"></td></tr>
-            @error('age')
-                <tr><th>ERROR</th>
-                <td>{{$message}}</td></tr>
-            @enderror
-            <tr><th>age: </th><td><input type="text"
-                name="age" value="{{old('age')}}"></td></tr>
+            @if ($errors->has('msg'))
+            <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
+            @endif
+            <tr><th>Message: </th><td><input type="text" name="msg"
+                value="{{old('msg')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="send">
-            </td></tr>
+                </td></tr>
+        
         </table>
         </form>
 @endsection

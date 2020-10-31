0ab3549d573a9761f2ba2ab33e2b0451c46c4b64
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-10-31 11:45:29 +0900

    #201029-laravel-practice

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -17,31 +17,15 @@
 
 @section('content')
     <table>
-        <tr>
-        <th>Name</th>Mail</th><th>Age</th></tr>
-        @foreach ($item as $item)
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+        @foreach ($items as $item)
             <tr>
                 <td>{{$item->name}}</td>
                 <td>{{$item->mail}}</td>
-                <td>{{$item->age}}</td>
+                
+                
             </tr>
         @endforeach
     </table>
-        <!-- <p>{{$msg}}</p>
-        @if (count($errors) > 0)
-            <p>入力に問題があります。再入力してください。</p>
-        @endif
-        <form action="/hello" method="post">
-        <table>
-            @csrf
-            @if ($errors->has('msg'))
-            <tr><th>ERROR</th><td>{{$errors->first('msg')}}</td></tr>
-            @endif
-            <tr><th>Message: </th><td><input type="text" name="msg"
-                value="{{old('msg')}}"></td></tr>
-            <tr><th></th><td><input type="submit" value="send">
-                </td></tr>
         
-        </table>
-        </form> -->
 @endsection

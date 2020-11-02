753d06d0a3d9aef4e309aa55a1ce65109f44a438
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-02 10:32:48 +0900

    #201102-laravel-practice

--- routes/web.php
+++ routes/web.php
@@ -62,3 +62,5 @@ Route::post('hello/edit', 'HelloController@update');
 
 Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
+
+Route::get('hello/show', 'HelloController@show');

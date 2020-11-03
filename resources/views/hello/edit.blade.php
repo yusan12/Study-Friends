c9a226d00090609168f5b714fab3a27b29f04cad
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-03 11:15:18 +0900

    #201103-laravel-practice

--- resources/views/hello/edit.blade.php
+++ resources/views/hello/edit.blade.php
@@ -13,8 +13,9 @@
         <input type="hidden" name="id" value="{{$form->id}}">
         <tr><th>name: </th><td><input type="text" name="name"
             value="{{$form->name}}"></td></tr>
-        <tr><th>mail: </th><td><input type="text" name="age"
-            value="send"></td></tr>
+        <tr><th>mail: </th><td><input type="text" name="mail"
+            value="{{$form->mail}}"></td></tr>
+        <tr><th></th><td><input type="submit" value="send"></td></tr>
     </table>
     </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,5 @@ Route::get('hello/del', 'HelloController@del');
 Route::post('hello/del', 'HelloController@remove');
 
 Route::get('hello/show', 'HelloController@show');
+
+Route::get('person', 'PersonController@index');

4bbc2e7b3c997b5119a8598a1cb8a9c862077eef
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-04 10:17:04 +0900

    #201104-laravel-practice

--- resources/views/person/index.blade.php
+++ resources/views/person/index.blade.php
@@ -7,17 +7,17 @@
     インデックスページ
 @endsection
 
+
+
 @section('content')
-    <table>
-    <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
-    @foreach ($items as $item)
-        <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->mail}}</td>
-            <td>{{$item->age}}</td>
-        </tr>
-    @endforeach
-    </table>
+   <table>
+   <tr><th>Data</th></tr>
+   @foreach ($items as $item)
+       <tr>
+           <td>{{$item->getData()}}</td>
+       </tr>
+   @endforeach
+   </table>
 @endsection
 
 @section('footer')
--- routes/web.php
+++ routes/web.php
@@ -66,3 +66,6 @@ Route::post('hello/del', 'HelloController@remove');
 Route::get('hello/show', 'HelloController@show');
 
 Route::get('person', 'PersonController@index');
+
+Route::get('person/find', 'PersonController@find');
+Route::post('person/find', 'PersonController@search');

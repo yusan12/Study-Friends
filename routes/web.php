083099e23d646851880782f0b6120cabe7f70580
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-06 09:43:26 +0900

    #201106-laravel-practice

--- routes/web.php
+++ routes/web.php
@@ -69,3 +69,14 @@ Route::get('person', 'PersonController@index');
 
 Route::get('person/find', 'PersonController@find');
 Route::post('person/find', 'PersonController@search');
+
+Route::get('person/add', 'PersonController@add');
+Route::post('person/add', 'PersonController@create');
+
+Route::get('person/edit', 'PersonController@edit');
+Route::post('person/edit', 'PersonController@update');
+
+Route::get('person/del', 'PersonController@delete');
+Route::post('person/del', 'PersonController@remove');
+
+

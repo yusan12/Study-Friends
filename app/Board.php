c8f21a9103fe813e38db00f1689c5224c30ff97c
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-07 11:00:07 +0900

    #201107-laravel-practice

---
+++ app/Board.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Board extends Model
+{
+    
+    protected $guarded = array('id');
+
+        public static $rules = array(
+            'person_id' => 'required',
+            'title' => 'required',
+            'message' => 'required'
+        );
+
+    public function getData()
+    {
+        return $this->id . ': ' . $this->title;
+    }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -79,4 +79,8 @@ Route::post('person/edit', 'PersonController@update');
 Route::get('person/del', 'PersonController@delete');
 Route::post('person/del', 'PersonController@remove');
 
+Route::get('board', 'BoardController@index');
+
+Route::get('board/add', 'BoardController@add');
+Route::post('board/add', 'BoardController@create');
 

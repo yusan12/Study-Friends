a010df036435820b9e583f4060a9a864850b140f
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-11 11:30:24 +0900

    #201111-laravel-practice

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -86,4 +86,17 @@ class HelloController extends Controller
     {
         return view('hello.rest');
     }
+
+    public function ses_get(Request $request)
+    {
+        $sesdata = $request->session()->get('msg');
+        return view('hello.session', ['session_data' => $sesdata]);
+    }
+
+    public function ses_put(Request $request)
+    {
+        $msg = $request->input;
+        $request->session()->put('msg', $msg);
+        return redirect('hello/session');
+    }
 }

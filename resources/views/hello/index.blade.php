675ce2540732e5baaaa6e94c4e9a4244435748da
Yu Suzuki <haraheri7212@yahoo.co.jp>
2020-11-12 10:43:11 +0900

    #201112-laravel-practice

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,7 +9,12 @@
 </head>
 <body> -->
     @extends('layouts.helloapp')
+    <style>
+        .pagination { font-size:10pt; }
+        .pagination li { display:inline-block }
+    </style>
     @section('title', 'Index')
+
     @section('menubar')
         @parent
         インデックスページ
@@ -22,10 +27,13 @@
             <tr>
                 <td>{{$item->name}}</td>
                 <td>{{$item->mail}}</td>
-                
-                
+                <td>{{$item->age}}</td>
             </tr>
         @endforeach
     </table>
-        
+    {{ $items->links() }}
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
 @endsection
